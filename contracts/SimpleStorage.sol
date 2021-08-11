4fdde6d54db12b5e90d5b773b4b1eb0fdee1c208
Berkay <67913214+berkayermis@users.noreply.github.com>
2021-08-11 10:07:32 +0300

    final

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.0;
 contract SimpleStorage {
   string public storedData;
 
-  constructor() public {
+  constructor() payable {
     storedData = "First set a new employee of the month!";
   }
 
